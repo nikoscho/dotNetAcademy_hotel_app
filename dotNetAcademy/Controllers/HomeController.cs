00dff6ecf303324019360948d2b73ebf9788b4b7
NikosCho <nikoscholis@gmail.com>
2019-03-06 20:34:07 +0200

    reorganizing, menu active links

--- dotNetAcademy/Controllers/HomeController.cs
+++ dotNetAcademy/Controllers/HomeController.cs
@@ -31,44 +31,8 @@ namespace dotNetAcademy.Controllers
             return View();
         }
 
-        public IActionResult Login() {
-            return View();
-        }
 
-        [Authorize]
-        public IActionResult Profile() {
 
-            string username = User.Identity.Name;
-
-            var useid = this.User.Identity.IsAuthenticated ? int.Parse(this.User.FindFirst(ClaimTypes.NameIdentifier).Value) : -1;
-
-                var rooms = _db.Room
-                .Include(t => t.RoomType)
-                .Include(r => r.Reviews)
-                .Include(b => b.Bookings)
-                .AsQueryable();
-
-            var user_bookings = _db.Bookings
-                .Where(booking => booking.UserId == useid).AsQueryable();
-
-            rooms = rooms.Where(i => user_bookings.Any(b => b.RoomId == i.RoomId));
-
-            var favorites = _db.Favorites
-                .Include(t => t.Room)
-                .Where( o => o.UserId == useid);
-
-            var reviews = _db.Reviews
-                .Include(t => t.Room)
-                .Where(o => o.UserId == useid);
-
-            ProfileViewModel model = new ProfileViewModel {
-                Rooms = rooms.AsEnumerable(),
-                Reviews = reviews,
-                Favorites = favorites,
-            };
-
-            return View(model);
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- dotNetAcademy/Controllers/UserController.cs
+++ dotNetAcademy/Controllers/UserController.cs
@@ -1,31 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using dotNetAcademy.Models;
 using dotNetAcademy.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace dotNetAcademy.Controllers
 {
     public class UserController : Controller
     {
+        private readonly WdaContext _db;
         private readonly SignInManager<User> _signInManager;
 
-        public UserController(SignInManager<User> signInManager) {
+        public UserController(WdaContext db, SignInManager<User> signInManager) {
+            this._db = db;
             this._signInManager = signInManager;
         }
 
-        [HttpPost]
-        public IActionResult Login(LoginViewModel model)
+        [HttpGet]
+        public IActionResult Login() {
+            return View();
+        }
+
+        [HttpPost, ActionName("Login")]
+        public IActionResult SignIn(LoginViewModel model)
         {
             if (ModelState.IsValid) {
 
                 var result = _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, false).Result;
 
                 if (result.Succeeded) {
-                    return RedirectToAction("Profile", "Home");
+                    return RedirectToAction("Profile", "User");
                 } else {
                     return RedirectToAction("Index", "Home");
                 }
@@ -45,6 +57,41 @@ namespace dotNetAcademy.Controllers
             }
 
             return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize]
+        public IActionResult Profile() {
+
+            string username = User.Identity.Name;
+
+            var useid = this.User.Identity.IsAuthenticated ? int.Parse(this.User.FindFirst(ClaimTypes.NameIdentifier).Value) : -1;
+
+            var rooms = _db.Room
+            .Include(t => t.RoomType)
+            .Include(r => r.Reviews)
+            .Include(b => b.Bookings)
+            .AsQueryable();
+
+            var user_bookings = _db.Bookings
+                .Where(booking => booking.UserId == useid).AsQueryable();
+
+            rooms = rooms.Where(i => user_bookings.Any(b => b.RoomId == i.RoomId));
+
+            var favorites = _db.Favorites
+                .Include(t => t.Room)
+                .Where(o => o.UserId == useid);
+
+            var reviews = _db.Reviews
+                .Include(t => t.Room)
+                .Where(o => o.UserId == useid);
+
+            ProfileViewModel model = new ProfileViewModel {
+                Rooms = rooms.AsEnumerable(),
+                Reviews = reviews,
+                Favorites = favorites,
+            };
+
+            return View(model);
         }
     }
 
